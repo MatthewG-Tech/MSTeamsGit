0f9c56d97a360ee1f486ea4442c3cfdf7fa6d9ae
Matthew <matthewbguerra@yahoo.com>
2021-01-31 10:34:25 -0800

    Finish message formatter

--- GitMaster/Message/message.js
+++ GitMaster/Message/message.js
@@ -1,5 +1,11 @@
 // Copyright (c) Matthew Guerra. All rights reserved.
 // Licensed under the MIT License.
+MessageAction = {
+    PUSH: "push",
+    PIPELINE: "pipeline",
+    COMMENT: "comment",
+    TASKCOMPLETE: "task completed"
+}
 
 class Message {
     constructor(object) {
@@ -9,6 +15,28 @@ class Message {
         this.date = object.date;
         this.user = object.user;
         this.owner = object.owner;
+        this.repo = object.repo;
+    }
+
+    getFormattedMessage() {
+        switch(this.action){
+            case(MessageAction.PUSH):
+                return this.createMessage(`Code has been push by ${this.user} to ${this.repo}`, this.user);
+                break;
+            case(MessageAction.PIPELINE):
+                return this.createMessage(`Pipeline ${this.status} on ${this.repo}`);
+                break;
+            case(MessageAction.COMMENT):
+                return this.createMessage(`Comment made by ${this.user}`, this.user);
+                break;
+            case(MessageAction.TASKCOMPLETE):
+                return this.createMessage(`A task has been completed by ${this.user}`, this.user);
+                break;
+        }
+    }
+
+    createMessage(message, user) {
+        return { text: message, user: user };
     }
 }
 
--- GitMaster/test/Message/message.test.js
+++ GitMaster/test/Message/message.test.js
@@ -1,17 +1,62 @@
 const assert = require('assert');
+const { MessageReactionTypes } = require('botbuilder');
 const sinon = require('sinon');
 const { Message } = require('../../Message/message.js');
 
-var defaultMessage = { id: "test", action: "run", status: "status", date: new Date(), user: "user", owner: "owner" };
+var defaultMessageInput = { id: "test", action: MessageAction.PUSH, status: "status", date: new Date(), user: "user", owner: "owner", repo: "repo" };
 
 describe('Test Message Formatter', () => {
     it('Constructor should properly make a message', () => {
-        var message = new Message(object);
-        assert.strictEqual(object.id, message.id);
-        assert.strictEqual(object.action, message.action);
-        assert.strictEqual(object.status, message.status);
-        assert.strictEqual(object.date, message.date);
-        assert.strictEqual(object.user, message.user);
-        assert.strictEqual(object.owner, message.owner);
+        var message = new Message(defaultMessageInput);
+        assert.strictEqual(defaultMessageInput.id, message.id);
+        assert.strictEqual(defaultMessageInput.action, message.action);
+        assert.strictEqual(defaultMessageInput.status, message.status);
+        assert.strictEqual(defaultMessageInput.date, message.date);
+        assert.strictEqual(defaultMessageInput.user, message.user);
+        assert.strictEqual(defaultMessageInput.owner, message.owner);
+        assert.strictEqual(defaultMessageInput.repo, message.repo);
+
+    });
+
+    it('Get push action forrmatted message', () => {
+        defaultMessageInput.action = MessageAction.PUSH;
+        var message = new Message(defaultMessageInput);
+        var formattedMessage = message.getFormattedMessage();
+        var staticText = `Code has been push by ${defaultMessageInput.user} to ${defaultMessageInput.repo}`
+        assert.strictEqual(formattedMessage.text, staticText);
+        assert.strictEqual(formattedMessage.user, defaultMessageInput.user);
+    });
+
+    it('Get pipeline passed action forrmatted message', () => {
+        defaultMessageInput.action = MessageAction.PIPELINE;
+        defaultMessageInput.status = "passed";
+        var message = new Message(defaultMessageInput);
+        var formattedMessage = message.getFormattedMessage();
+        assert.strictEqual(formattedMessage.text, `Pipeline ${defaultMessageInput.status} on ${defaultMessageInput.repo}`);
+    });
+
+    it('Get pipeline failed action forrmatted message', () => {
+        defaultMessageInput.action = MessageAction.PIPELINE;
+        defaultMessageInput.status = "failed";
+        var message = new Message(defaultMessageInput);
+        var formattedMessage = message.getFormattedMessage();
+        assert.strictEqual(formattedMessage.text, `Pipeline ${defaultMessageInput.status} on ${defaultMessageInput.repo}`);
+    });
+
+
+    it('Get comment action forrmatted message', () => {
+        defaultMessageInput.action = MessageAction.COMMENT;
+        var message = new Message(defaultMessageInput);
+        var formattedMessage = message.getFormattedMessage();
+        assert.strictEqual(formattedMessage.text, `Comment made by ${defaultMessageInput.user}`);
+        assert.strictEqual(formattedMessage.user, defaultMessageInput.user);
+    });
+
+    it('Get task complete action forrmatted message', () => {
+        defaultMessageInput.action = MessageAction.TASKCOMPLETE;
+        var message = new Message(defaultMessageInput);
+        var formattedMessage = message.getFormattedMessage();
+        assert.strictEqual(formattedMessage.text, `A task has been completed by ${defaultMessageInput.user}`);
+        assert.strictEqual(formattedMessage.user, defaultMessageInput.user);
     });
 });
